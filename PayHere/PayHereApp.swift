bc51697daa2fe9604bba9fdb28274f8e3c2b52ef
DinukaShehan99 <shehan.shyaminda@gmail.com>
2022-02-11 21:03:35 +0530

    Initial Commit

---
+++ PayHere/PayHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayHereApp.swift
+//  PayHere
+//
+//  Created by Dinuka Shehan on 2022-02-11.
+//
+
+import SwiftUI
+
+@main
+struct PayHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
